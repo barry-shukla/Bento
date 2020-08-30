5341f4971f43aca1ff0692e8c32a32769696e8e5
Barry <gshukla209@gmail.com>
2020-08-30 17:04:18 -0400

    Changed name and tiiiime to sleep

--- js/greeting.js
+++ js/greeting.js
@@ -2,15 +2,15 @@ var today = new Date();
 var Hr = today.getHours();
 
 // Here you can change the greetings and your name
-var name = 'John Doe';
+var name = 'Barry Shukla';
 var lateTxt = 'Go to Sleep! ';
 var morningTxt = 'Good morning! ';
 var afterTxt = 'Good afternoon ';
 var evenTxt = 'Good evening ';
 
-if (Hr >= 0 && Hr < 6) {
+if (Hr >= 2 && Hr < 10) {
     document.getElementById('greetings').innerText = lateTxt + name;
-} else if (Hr >= 6 && Hr < 12) {
+} else if (Hr >= 10 && Hr < 12) {
     document.getElementById('greetings').innerText = morningTxt + name;
 } else if (Hr >= 12 && Hr < 17) {
     document.getElementById('greetings').innerText = afterTxt + name;
